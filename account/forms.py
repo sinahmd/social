9b1ba4c872a9fd9524d82de6182e861da6c4d064
sin <sinahamidi8@gmail.com>
2023-10-16 23:54:23 +0330

    add login,logout

--- account/forms.py
+++ account/forms.py
@@ -4,7 +4,9 @@ from django.core.exceptions import ValidationError
 class UserRegistrationForm(forms.Form):
     username = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}))
     email = forms.EmailField(widget=forms.EmailInput(attrs={'class':'form-control'}))
-    password = forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control'}))
+    password1 = forms.CharField(label='password', widget=forms.PasswordInput(attrs={'class':'form-control'}))
+    password2 = forms.CharField(label='confirm password', widget=forms.PasswordInput(attrs={'class':'form-control'}))
+
     def clean_email(self):
         email = self.cleaned_data['email']
         user = User.objects.filter(email=email).exists()
@@ -12,8 +14,18 @@ class UserRegistrationForm(forms.Form):
             raise ValidationError('this email already exists')
         return email
     def clean_username(self):
-        username = self.cleaned_data['username1']
+        username = self.cleaned_data['username']
         user = User.objects.filter(username=username).exists()
         if user:
             raise ValidationError('user exists')
         return username
+    def clean(self):
+        cd = super().clean()
+        pas1 = cd.get('password1')
+        pas2 = cd.get('password2')
+        if pas1 and pas2 and pas1 != pas2:
+            raise ValidationError('PASSWORD MUST MATCH')
+
+class UserLoginForm(forms.Form):
+    username = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control'}))
--- account/views.py
+++ account/views.py
@@ -1,21 +1,46 @@
 from django.shortcuts import render, redirect
 from django.views import View
-from .forms import UserRegistrationForm
+from .forms import UserRegistrationForm, UserLoginForm
 from django.contrib.auth.models import User
 from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
 # Create your views here.
 
-class RegisterView(View):
+class UserRegisterView(View):
     form_class = UserRegistrationForm
-    tempfile_name = 'account/register.html'
+    template_name = 'account/register.html'
     def get(self, request):
         form = self.form_class()
-        return render(request, self.tempfile_name, {'form':form})
+        return render(request, self.template_name, {'form':form})
     def post(self, request):
         form = self.form_class(request.POST)
         if form.is_valid():
             cd = form.cleaned_data
-            User.objects.create_user(cd['username'], cd['email'], cd['password'])
+            User.objects.create_user(cd['username'], cd['email'], cd['password2'])
             messages.success(request, 'done', 'success')
             return redirect('home:home')
-        return render(request, self.tempfile_name, {'form': form})
+        return render(request, self.template_name, {'form': form})
+
+class UserLoginView(View):
+    form_class = UserLoginForm
+    template_name = 'account/login.html'
+    def get(self, request):
+        form = self.form_class
+        return render(request, self.template_name, {'form':form})
+    def post(self, request):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(request, username=cd['username'], password=cd['password'])
+            if user is not None:
+                login(request, user)
+                messages.success(request,'loged in', 'success')
+                return redirect('home:home')
+            messages.error(request, 'wrong password or username', 'danger')
+        return render(request, self.template_name, {'form':form})
+
+class UserLogoutView(View):
+    def get(self, request):
+        logout(request)
+        messages.success(request,'loged out', 'success')
+        return redirect('home:home')
