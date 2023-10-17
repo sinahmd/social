267bd3866e2ccf47d74edfd91deef479f9a0d91f
sin <sinahamidi8@gmail.com>
2023-10-17 19:49:02 +0330

    add custom authenticate

--- account/views.py
+++ account/views.py
@@ -4,11 +4,17 @@ from .forms import UserRegistrationForm, UserLoginForm
 from django.contrib.auth.models import User
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.mixins import LoginRequiredMixin
 # Create your views here.
 
 class UserRegisterView(View):
+
     form_class = UserRegistrationForm
     template_name = 'account/register.html'
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect('home:home')
+        return super().dispatch(request,*args, **kwargs)
     def get(self, request):
         form = self.form_class()
         return render(request, self.template_name, {'form':form})
@@ -24,6 +30,10 @@ class UserRegisterView(View):
 class UserLoginView(View):
     form_class = UserLoginForm
     template_name = 'account/login.html'
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect('home:home')
+        return super().dispatch(request, *args, **kwargs)
     def get(self, request):
         form = self.form_class
         return render(request, self.template_name, {'form':form})
@@ -39,7 +49,8 @@ class UserLoginView(View):
             messages.error(request, 'wrong password or username', 'danger')
         return render(request, self.template_name, {'form':form})
 
-class UserLogoutView(View):
+class UserLogoutView(LoginRequiredMixin, View):
+
     def get(self, request):
         logout(request)
         messages.success(request,'loged out', 'success')
